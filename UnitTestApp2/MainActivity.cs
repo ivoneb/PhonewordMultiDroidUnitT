3fb488e5f8aa304681c8d89fbf717a05f9a18951
Ivone B <ivonebennett@gmail.com>
2017-10-15 08:39:09 +0800

    initial version

---
+++ UnitTestApp2/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace UnitTestApp2
+{
+    [Activity(Label = "UnitTestApp2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            // tests can be inside the main assembly
+            //AddTest(Assembly.GetExecutingAssembly());
+            // or in any reference assemblies
+            // AddTest (typeof (Your.Library.TestClass).Assembly);
+            AddTest(typeof(UnitTestApp2.TestsSample).Assembly);
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+    }
+}
+
---
+++ UnitTestApp2/TestSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using PhonewordPCL;
+
+namespace UnitTestApp2
+{
+    [TestFixture]
+    public class TestsSample
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [TearDown]
+        public void Tear() { }
+
+        [Test]
+        public void Test_phonewordPassMethod()
+        {
+            Console.WriteLine("test1 phoneword translator");
+            string output = PhonewordTranslator.ToNumber("ABCDEF");
+            Assert.AreEqual("222333", output);
+        }
+
+        [Test]
+        public void Test_phonewordFailMethod()
+        {
+            Console.WriteLine("test2 phoneword translator");
+            string output = PhonewordTranslator.ToNumber("BCDEFG");
+            Assert.AreEqual("223335", output);
+        }
+
+    }
+}
